87c1fcc39e80bf2c8f04a48bcb9394ed5ab78a44
kaelsilva <kael.leak1998@gmail.com>
2019-12-17 20:57:37 -0300

    Adicionando tela e funcionalidades de cálculo do IMC. #3

--- hunt/App.js
+++ hunt/App.js
@@ -1,12 +1,56 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, TextInput, View, TouchableOpacity} from 'react-native';
 
 export default class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      altura: 0,
+      massa: 0,
+      resultado: 0,
+      resultadoText: '',
+    }
+    this.calcular = this.calcular.bind(this);
+  }
+
+  calcular(){
+    let imc = this.state.massa/(this.state.altura * this.state.altura);
+    let s = this.state;
+    s.resultado = imc;
+
+    if (s.resultado < 16)
+      s.resultadoText = 'Magreza grave';
+    else if (s.resultado >= 16 && s.resultado < 17)
+      s.resultadoText = 'Magreza moderada';
+    else if (s.resultado >= 17 && s.resultado < 18.5)
+      s.resultadoText = 'Magreza leve';
+    else if (s.resultado >= 18.5 && s.resultado < 25)
+      s.resultadoText = 'Saudável';
+    else if (s.resultado >= 25 && s.resultado < 30)
+      s.resultadoText = 'Sobrepeso';
+    else if (s.resultado >= 30 && s.resultado < 35)
+      s.resultadoText = 'Obesidade Grau I';
+    else if (s.resultado >= 35 && s.resultado < 40)
+      s.resultadoText = 'Obesidade Grau II (severa)';
+    else if (s.resultado >= 40)
+      s.resultadoText = 'Obesidade Grau III (mórbida)';
+
+
+    this.setState(s);
+  }
+
+
+
   render(){
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <View style={styles.box} />
+        <View style={styles.entradas}>
+          <TextInput placeholder='Altura' keyboardType='phone-pad' style={styles.input} onChangeText={(altura)=>{this.setState({altura})}}/>
+          <TextInput placeholder='Massa' keyboardType='numeric' style={styles.input} onChangeText={(massa)=>{this.setState({massa})}}/>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+        <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+        <Text style={[styles.resultado, {fontSize: 35}]}>{this.state.resultadoText}</Text>
       </View>
     );
   }
@@ -15,18 +59,32 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
+  input: {
+    height: 80,
     textAlign: 'center',
-    margin: 10,
+    width: '50%',
+    fontSize: 50,
+    marginTop: 24,
   },
-  box: {
-    width: 60,
-    height: 60,
-    backgroundColor: '#F00',
+  button: {
+    backgroundColor: '#89FFA5',
+  },
+  entradas: {
+    flexDirection: 'row',
+  },
+  buttonText: {
+    alignSelf: 'center',
+    padding: 30,
+    fontSize: 27,
+    color: '#33aa33',
+    fontWeight: 'bold',
+  },
+  resultado: {
+    alignSelf: 'center',
+    color: '#cccccf',
+    fontSize: 65,
+    padding: 15,
   }
 });
